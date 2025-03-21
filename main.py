d54ec44d017c1a53cb225c92153fedfbd54f7269
JimTPRI <309698758@qq.com>
2025-03-21 23:25:49 +0800

    Create columns, image, and description.

---
+++ main.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Ardit Sulce")
+    content = """
+    一氧化碳能够吸收地球表面释放的长波红外辐射，因此符合温室气体的定义。然而，其浓度在大气
+    中极低（通常不超过0.01%），直接吸收的热量有限，因此对全球变暖的直接影响微弱。\n
+    一氧化碳在严格定义上属于温室气体，但其对全球变暖的直接贡献有限，更多是通过影响其他
+    温室气体的循环间接发挥作用。当前气候变化应对措施仍以减排二氧化碳、甲烷等气体为核心，
+    但CO的间接效应仍需在大气化学模型中纳入考量。
+    """
+    st.info(content)
